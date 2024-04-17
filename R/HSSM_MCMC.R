3507e070043a03119a263083ecc8d38eac87434b
Alessandro Colombi <colombi.alessandro@hotmail.it>
2024-04-17 17:42:58 +0200

    R sampler wrapper implemented. ready to test

--- R/HSSM_MCMC.R
+++ R/HSSM_MCMC.R
@@ -28,7 +28,8 @@ set_options_sampler = function( X_j = NULL,
   b_gamma = a_gamma*exp(-X_j*beta0)
   res = list( "X_j" = X_j,
               "gamma0" = gamma0, "beta0" = beta0,"Lambda0" = Lambda0,
-              "UpdateLambda" = UpdateLambda, "UpdateGamma" = UpdateGamma, "UpdateBeta" = UpdateBeta,
+              "UpdateLambda" = UpdateLambda, "UpdateGamma" = UpdateGamma,
+              "UpdateBeta" = UpdateBeta,
               "use_covariates" = use_covariates,
               "L0" = L0, "V_Lambda" = V_Lambda,
               "sigma2" = sigma2,
@@ -36,4 +37,20 @@ set_options_sampler = function( X_j = NULL,
               "adapt_var_beta" = adapt_var_beta, "a_gamma" = a_gamma, "b_gamma" = b_gamma,
               "M_max" = M_max)
   return(res)
+}
+
+#' Run MCMC Sampler
+#'
+#' @export
+MCMC_Sampler(counts_jk,niter,nburn = 0,thin = 1,option){
+  d = nrow(counts_jk)
+  r = ncol(counts_jk)
+  n =  sum(counts_jk)
+  n_j = c(apply(counts_jk,1,sum))
+  n_K = c(apply(counts_jk,2,sum))
+  if(any(n_K) == 0)
+    stop("Error: there are empty columns in counts_jk")
+  ## Qui checks se vuoi
+
+  return( MCMC_Sampler_c(counts_jk,n_j,r,niter,nburn,thin,option) )
 }
